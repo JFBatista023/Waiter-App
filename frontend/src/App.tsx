2604b15c0c7c6aa1f91696d643ffd55f378d6a4f
JFBatista023 <joaofilipeb045@gmail.com>
2022-12-02 23:04:22 -0300

    Create Order component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { GlobalStyles } from "./styles/GlobalStyles";
 
 import { Header } from "./components/Header";
+import { Order } from "./components/Order";
 
 export function App() {
     return (
         <>
             <GlobalStyles />
             <Header />
+            <Order />
         </>
     );
 }
